b8362d2ef2d0d64947a991a16bf8a7cdb2cb1deb
Roman Cherkasov <palkamon@yandex.ru>
2021-11-16 16:03:20 +0700

    create first user model

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+
+    CHOICES = (
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+    )
+
+    first_name = models.CharField(
+        verbose_name='first name',
+        max_length=150,
+        blank=False
+    )
+    
+    last_name = models.CharField(
+        verbose_name='last name',
+        max_length=150,
+        blank=False
+    )
+    
+    email = models.EmailField(
+        verbose_name='email address',
+        blank=False
+    )
+
+    role = models.CharField(
+        verbose_name='User Role',
+        max_length=16,
+        null=False,
+        default=USER,
+        choices=CHOICES,
+    )
