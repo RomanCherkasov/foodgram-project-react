f4c43c88d87207fa1b6b72494e886dfe381a2b47
Roman Cherkasov <palkamon@yandex.ru>
2021-11-17 15:42:52 +0700

    create serializer for user and registration

---
+++ backend/app/users/serializers.py
@@ -0,0 +1,30 @@
+from django.db import models
+from rest_framework import serializers
+
+from users.models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(
+        max_length=10,
+        default='user',
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'username',
+            'role',
+        )
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'username',
+            'email',
+            'role',
+            'first_name',
+            'last_name',
+        )
