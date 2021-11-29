14d0aebcd547de5ec221e9421f275cb5f5bb1913
Roman Cherkasov <palkamon@yandex.ru>
2021-11-29 22:11:25 +0700

    create methods for get sub and unsub

--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST,
-HTTP_201_CREATED)
+HTTP_201_CREATED, HTTP_204_NO_CONTENT)
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
 from rest_framework.generics import get_object_or_404
 from users.serializers import SubscribeSerializer
 from users.models import IsSubscribed
@@ -12,6 +13,14 @@ from users.models import IsSubscribed
 User = get_user_model()
 
 class UserViewSet(UserViewSet):
+    def allsubs(self, request):
+        user = request.user
+        queryset = IsSubscribed.objects.filter(user=user)
+        serializer = SubscribeSerializer(many=True,
+        context={'request': request})
+        return Response(serializer.data)
+
+    @action(detail=True, permission_classes=[IsAuthenticated])
     def sub(self, request, id=None):
         user = request.user
         author = get_object_or_404(User, id=id)
@@ -29,3 +38,17 @@ class UserViewSet(UserViewSet):
             }
         )
         return Response(serializer.data, status=HTTP_201_CREATED)
+
+    # https://stackoverflow.com/questions/62084905/how-to-make-delete-method-in-django-extra-action
+    @sub.mapping.delete
+    def unsub(self, request, id=None):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+        if user == author:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+        subscribe = IsSubscribed.objects.create(user=user, author=author)
+        if subscribe.exsist():
+            subscribe.delete()
+            return Response({}, status=HTTP_204_NO_CONTENT)
+
+        return Response({}, status=HTTP_400_BAD_REQUEST)
