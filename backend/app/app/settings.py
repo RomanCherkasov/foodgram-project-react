0e190fe84abbe34ee5cdaa0dc4440567392624f4
Roman Cherkasov <palkamon@yandex.ru>
2021-11-29 18:17:46 +0700

    try to new database

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -144,6 +144,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.user'
+# AUTH_USER_MODEL = 'users.user'
 
 DJOSER = djoser_settings
