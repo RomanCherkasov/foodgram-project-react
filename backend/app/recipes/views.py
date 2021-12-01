94d9d475910a5bd381762e1378f2c04d8c953d8e
Roman Cherkasov <palkamon@yandex.ru>
2021-12-01 15:26:47 +0700

    cleaned up a litle

--- backend/app/recipes/views.py
+++ backend/app/recipes/views.py
@@ -23,38 +23,18 @@ class IngredientViewSet(viewsets.ModelViewSet):
     search_fields = ('^name',)
     lookup_url_kwarg = 'id'
 
-    def get_object(self):
-        id = self.kwargs.get(self.lookup_url_kwarg)
-        return get_object_or_404(Ingredients, id=id)
-
-    def get_queryset(self):
-        return self.queryset
-
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [RegistredUser]
-    # lookup_url_kwarg = 'id'
-    
-    # def get_queryset(self):
-    #     return self.queryset
 
-    # def get_object(self):
-    #     id = self.kwargs.get(self.lookup_url_kwarg)
-    #     return get_object_or_404(Tag, id=id)
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipesSerializer
     permission_classes = [RegistredUser]
     pagination_class = Paginator
-    def get_queryset(self):
-        return self.queryset
-
-    def get_object(self):
-        id = self.kwargs.get(self.lookup_url_kwarg)
-        return get_object_or_404(Recipe, id=id)
-
+    # TODO CREATE FILTRES
     def perform_create(self, serializer):
         print(self.request.data.get('tags')[0])
         serializer.save(
