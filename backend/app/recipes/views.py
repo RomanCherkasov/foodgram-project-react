a5e9e315ad5576d60413fb36c2695d63a99afbbc
Roman Cherkasov <palkamon@yandex.ru>
2021-12-01 15:18:52 +0700

    create methods for add recipe to favorite and to cart. don't work fine, need to rework

--- backend/app/recipes/views.py
+++ backend/app/recipes/views.py
@@ -2,13 +2,15 @@ from django.db.models.query import QuerySet
 from django.http import request
 from django.shortcuts import get_object_or_404, render
 from rest_framework import serializers, status, viewsets
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
-
+from users.models import Favorite, Cart
+from users.serializers import FavoriteRecipeSerializer
 from recipes.models import Ingredients, Recipe, Tag
 from recipes.serializers import (IngidientsSerializer, RecipesSerializer,
                                  TagSerializer)
 from users.permissions import RegistredUser
+from rest_framework.permissions import IsAuthenticated
 from app.filters import IngredientsFilter
 
 
@@ -52,12 +54,58 @@ class RecipeViewSet(viewsets.ModelViewSet):
         id = self.kwargs.get(self.lookup_url_kwarg)
         return get_object_or_404(Recipe, id=id)
 
-    # def create(self, request):
-    #     print(request.data.get('ingredients'))
-    #     return Response(request.data)
-
     def perform_create(self, serializer):
         print(self.request.data.get('tags')[0])
         serializer.save(
             author=self.request.user,
         )
+
+    @action(
+        detail=True,
+        methods=['get', 'delete'],
+        permission_classes=[IsAuthenticated]
+    )
+    def favorite_add(self, request, id): #TODO maybe create method for add and delete
+        if request.method == 'GET':
+            if Favorite.objects.filter(
+                user=request.user, recipe__id=id).exists():
+                return Response({'error':'Recipe in favorite already'},
+                status=status.HTTP_400_BAD_REQUEST)
+            recipe = get_object_or_404(Recipe, id=id)
+            Favorite.objects.create(user=request.user, recipe=recipe)
+            serializer = FavoriteRecipeSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == 'DELETE':
+            recipe = Favorite.objects.filter(user=request.user, recipe__id=id)
+            if recipe.exists():
+                recipe.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({'error':'Recipe deleted already'},
+            status=status.HTTP_400_BAD_REQUEST)
+        return None
+
+    @action(
+        detail=True,
+        methods=['get', 'delete'],
+        permission_classes=[IsAuthenticated]
+    )
+    def cart_add(self, request, id): #TODO maybe create method for add and delete
+        if request.method == 'GET':
+            if Cart.objects.filter(
+                user=request.user, recipe__id=id).exists():
+                return Response({'error':'Recipe in cart already'},
+                status=status.HTTP_400_BAD_REQUEST)
+            recipe = get_object_or_404(Recipe, id=id)
+            Cart.objects.create(user=request.user, recipe=recipe)
+            serializer = FavoriteRecipeSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == 'DELETE':
+            recipe = Cart.objects.filter(user=request.user, recipe__id=id)
+            if recipe.exists():
+                recipe.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({'error':'Recipe deleted already'},
+            status=status.HTTP_400_BAD_REQUEST)
+        return None
+
+
