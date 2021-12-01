c19d5353a07c977eeef828dd03652525b323eaf4
Roman Cherkasov <palkamon@yandex.ru>
2021-12-01 16:45:06 +0700

    okey roma davai po novoi

--- backend/app/app/djoser_settings.py
+++
@@ -1,8 +0,0 @@
-DJOSER = {
-    'LOGIN_FIELD': 'email',
-    'SERIALIZERS':{
-        'user_create': 'users.serializers.RegistrationSerializer',
-        'user': 'users.serializers.UserSerializer',
-        'current_user': 'users.serializers.UserSerializer',
-    }
-}
--- backend/app/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from rest_framework import routers
-
-from users.views import RegistrationAPIView
-
-router = routers.DefaultRouter()
-
-app_name = 'users'
