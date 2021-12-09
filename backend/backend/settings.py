047b3f9e419e7d2e361b485edb6f19fddbf1a801
Roman Cherkasov <palkamon@yandex.ru>
2021-12-09 07:52:31 +0700

    all works in docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         default='django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRESS_PASSWORD'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
