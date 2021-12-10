7d7ace0139d6a590a26a58a9fcf902b75338b561
Roman Cherkasov <palkamon@yandex.ru>
2021-12-11 01:09:12 +0700

    add verbose and verbose plural for all class and field in recipe models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -9,11 +9,18 @@ User = get_user_model()
 
 class Ingredient(models.Model):
     name = models.CharField(
-        max_length=128
+        max_length=128,
+        verbose_name='Название ингредиента'
     )
     measurement_unit = models.CharField(
-        max_length=128
+        max_length=128,
+        verbose_name='Единицы измерений'
     )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name='Ингредиент'
+        verbose_name_plural='Ингредиенты'
 
     def __str__(self):
         return self.name
@@ -23,20 +30,28 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         related_name='recipes',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
     )
     name = models.CharField(
-        max_length=128
+        max_length=128,
+        verbose_name='Название рецепта'
     )
     image = models.ImageField(
-        upload_to='recipes/'
+        upload_to='recipes/',
+        verbose_name='Изображение рецепта'
     )
     ingredients = models.ManyToManyField(
         Ingredient,
         through='IngredientsInRecipe',
+        verbose_name='Ингредиенты рецепта'
     )
-    text = models.TextField(default='Text')
-    tags = models.ManyToManyField('Tag')
+    text = models.TextField(
+        default='Text',
+        verbose_name='Текст рецепта')
+    tags = models.ManyToManyField(
+        'Tag',
+        verbose_name='Теги в рецепте')
     cooking_time = models.IntegerField(
         validators=(
             validators.MinValueValidator(
@@ -44,10 +59,13 @@ class Recipe(models.Model):
                 message='Min 1 minute'
             ),
         ),
+        verbose_name='Время готовки'
     )
 
     class Meta:
         ordering = ['-id']
+        verbose_name='Рецепт'
+        verbose_name_plural='Рецепты'
 
     def __str__(self):
         return self.name
@@ -56,12 +74,19 @@ class Recipe(models.Model):
 class Tag(models.Model):
     name = models.CharField(
         max_length=128,
-        unique=True)
-    color = ColorField('HEX Color', default='#000000')
-    slug = models.SlugField(max_length=128, unique=True)
+        unique=True,
+        verbose_name='Название тега')
+    color = ColorField('HEX цвет', default='#000000')
+    slug = models.SlugField(
+        max_length=128,
+        unique=True,
+        verbose_name='Слаг тега'
+        )
 
     class Meta:
         ordering = ['-id']
+        verbose_name='Тег'
+        verbose_name_plural='Теги'
 
     def __str__(self):
         return self.name
@@ -72,11 +97,13 @@ class IngredientsInRecipe(models.Model):
         'Ingredient',
         on_delete=models.CASCADE,
         related_name='ingredients_in_recipe',
+        verbose_name='Ингредиент'
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
         related_name='ingredients_in_recipe',
+        verbose_name='Рецепт'
     )
     amount = models.IntegerField(
         validators=(
@@ -85,9 +112,13 @@ class IngredientsInRecipe(models.Model):
                 message='Min 1 ingredient'
             ),
         ),
+        verbose_name='Количество ингредиентов'
     )
 
     class Meta:
+        ordering = ['-id']
+        verbose_name='Ингредиент в рецепте'
+        verbose_name_plural='Ингредиенты в рецепте'
         constraints = [
             models.UniqueConstraint(
                 fields=['recipe', 'ingredient'],
