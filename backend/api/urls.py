aa072b16e1906d53f1626087bd06329ddbe43b00
Roman Cherkasov <palkamon@yandex.ru>
2021-12-12 03:01:46 +0700

    some isort changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from djoser import views
+
 from rest_framework.routers import DefaultRouter
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
